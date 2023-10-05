ecb8f04da8a603a81cb78e9ce43616d7981de7bf
khj <developer.heejeong@gmail.com>
2023-10-05 18:21:22 +0900

    [2023.10.05] Entity 추가

--- src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
@@ -1,8 +1,15 @@
 package com.enicom.board.kyoritsu.dao.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity(name = "tb_content")
 @Builder
@@ -21,6 +28,28 @@ public class Content {
     @Column(name = "id")
     private String id;
 
+    @Column(name = "subject")
+    private String subject;
+
     @Column(name = "content")
     private String content;
+
+    @Column(name = "hit")
+    @Builder.Default
+    @ColumnDefault("0")
+    private Integer hit = 0;
+
+    @Column(name = "thumbnail_url", length = 100)
+    private String thumbnailUrl;
+
+    @Builder.Default
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime updateDate;
 }
--- src/main/java/com/enicom/board/kyoritsu/dao/entity/Menu.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/entity/Menu.java
@@ -1,11 +1,16 @@
 package com.enicom.board.kyoritsu.dao.entity;
 
 import com.enicom.board.kyoritsu.dao.type.MenuTarget;
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity(name = "tb_menu")
 @Builder
@@ -13,15 +18,12 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@SequenceGenerator(name = "SEQ_MENU_GENERATOR", sequenceName = "SEQ_MENU", initialValue = 1, allocationSize = 1)
 public class Menu {
     @Id
-    @Column(name = "code", length = 20)
-    private String code;
-
-    @ManyToOne
-    @JoinColumn(name = "p_code")
-    @Comment("상위 메뉴")
-    private Menu menu;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_MENU_GENERATOR")
+    @Column(name = "rec_key")
+    private Long recKey;
 
     @Column(name = "name", length = 20)
     private String name;
@@ -29,10 +31,8 @@ public class Menu {
     @Column(name = "url", length = 100)
     private String url;
 
-    @ManyToOne
-    @JoinColumn(name = "co_id")
-    @Comment("내용 아이디")
-    private Content content;
+    @Column(name = "thumbnail_url", length = 100)
+    private String thumbnailUrl;
 
     @Column(name = "target")
     @Builder.Default
@@ -42,9 +42,21 @@ public class Menu {
 
     @Column(name = "order_seq")
     @Builder.Default
+    @Comment("메뉴 보여질 순서 설정 - 오름차순 정렬")
     private Integer order = 0;
 
     @Column(name = "use_yn")
     @Builder.Default
     private Integer use = 1;
+
+    @Builder.Default
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime updateDate;
 }
