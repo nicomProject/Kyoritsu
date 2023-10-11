e0f7f6dc42e2202f6bf0d57d5ff09423a23e250b
khj <developer.heejeong@gmail.com>
2023-10-11 10:29:31 +0900

    [2023.10.10] Content Id 보완

--- src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
@@ -20,13 +20,10 @@ import java.time.LocalDateTime;
 @ToString
 @SequenceGenerator(name = "SEQ_CONTENT_GENERATOR", sequenceName = "SEQ_CONTENT", initialValue = 1, allocationSize = 1)
 public class Content {
+    @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CONTENT_GENERATOR")
     @Column(name = "rec_key")
     private Long recKey;
-
-    @Id
-    @Column(name = "id")
-    private String id;
 
     @Column(name = "title", length = 20)
     private String title;
