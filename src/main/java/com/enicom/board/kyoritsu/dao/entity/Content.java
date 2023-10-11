c94f69d2d7555c91a9d8590715242fb3918d6b04
R&D <jjg@enicom.co.kr>
2023-10-11 10:31:04 +0900

    [23.10.11] 수정

--- src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/entity/Content.java
@@ -20,6 +20,7 @@ import java.time.LocalDateTime;
 @ToString
 @SequenceGenerator(name = "SEQ_CONTENT_GENERATOR", sequenceName = "SEQ_CONTENT", initialValue = 1, allocationSize = 1)
 public class Content {
+    @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CONTENT_GENERATOR")
     @Column(name = "rec_key")
     private Long recKey;
