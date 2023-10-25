a15ef0cd50316964a6989c59c9b708eeb2643d33
cnh <cnh@enicom.co.kr>
2023-10-25 17:38:21 +0900

    [2023.10.25] (사용자페이지) 미사용 코드정리

--- src/main/resources/static/js/main/page/recruit/info.js
+++ src/main/resources/static/js/main/page/recruit/info.js
@@ -1,32 +1,9 @@
 $(function () {
     const Content = {
         load: function () {
-            Menu.load();
             this.event();
         },
         event: function () {
-            const $body = $('body');
-            const navbar = $('.navbar');
-
-            navbar.find('.navbar-close').on({
-                click: function (e) {
-                    $body.removeClass('g-sidenav-pinned');
-                }
-            });
-            navbar.find('.navbar-open').on({
-                click: function (e) {
-                    $body.addClass('g-sidenav-pinned');
-                }
-            });
-
-            navbar.find('.sidenav-button').on({
-                click: function (e) {
-                    const icon = $(this).find('i');
-                    icon.toggleClass('fa-compress');
-                    icon.toggleClass('fa-thumbtack');
-                    $body.toggleClass('g-sidenav-hidden');
-                }
-            })
         }
     }
 
