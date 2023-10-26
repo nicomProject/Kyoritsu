3aed098274222fab3162eca3e0afbf60017da2a7
cnh <cnh@enicom.co.kr>
2023-10-26 17:44:15 +0900

    [2023.10.26] (사용자페이지) 조직도 페이지 화면

--- src/main/resources/static/js/main/page/intro/organization.js
+++ src/main/resources/static/js/main/page/intro/organization.js
@@ -1,6 +1,39 @@
 $(function () {
     const Content = {
         load: function () {
+            this.draw();
+        },
+        draw: function () {
+            const container = $('#organization-section');
+            const editorContent = container.find('.editor-content');
+
+            let contentObj = [];
+
+            AjaxUtil.request({
+                url: '/api/introductions/find',
+                async: false,
+                success: function (data){
+                    contentObj = data.result.items;
+                }
+            })
+
+            console.log(contentObj)
+
+            if(contentObj != null){
+                contentObj.forEach(item => {
+                    const contentId = item.recKey;
+                    const contentData = item.content;
+
+                    console.log(contentData)
+
+                    const organizationContentId = $('#organization_contentId').val();
+
+                    if(contentId == organizationContentId){
+                        editorContent.html(contentData)
+                    }
+                })
+            }
+
             this.event();
         },
         event: function () {
