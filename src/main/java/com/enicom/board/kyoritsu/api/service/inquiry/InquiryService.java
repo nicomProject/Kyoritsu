97df420d689663b55133a3ff9555bbdaa8c373dc
khj <developer.heejeong@gmail.com>
2023-11-16 16:42:39 +0900

    [2023.11.06] Annotation 설정

--- src/main/java/com/enicom/board/kyoritsu/api/service/inquiry/InquiryService.java
+++ src/main/java/com/enicom/board/kyoritsu/api/service/inquiry/InquiryService.java
@@ -1,12 +1,9 @@
 package com.enicom.board.kyoritsu.api.service.inquiry;
 
 import com.enicom.board.kyoritsu.api.param.InquiryParam;
-import com.enicom.board.kyoritsu.api.param.IntroductionsParam;
-import com.enicom.board.kyoritsu.api.param.JobParam;
 import com.enicom.board.kyoritsu.api.param.type.MultipleParam;
 import com.enicom.board.kyoritsu.api.type.PageVO;
 import com.enicom.board.kyoritsu.api.type.ResponseDataValue;
-import com.enicom.board.kyoritsu.dao.entity.Content;
 import com.enicom.board.kyoritsu.dao.entity.Inquiry;
 
 public interface InquiryService {
@@ -14,6 +11,7 @@ public interface InquiryService {
     PageVO<?> findAll();
 
     PageVO<Inquiry> findAll(InquiryParam param);
+
     ResponseDataValue<?> add(InquiryParam param);
 
     ResponseDataValue<?> delete(MultipleParam param);
